bb995ff83cca75dacf09d83d5cea58d95b5eb0ea
Jonathan F-Carait <jonathan.carait@outlook.com>
2021-12-13 19:25:11 +1100

    changed lowerCase to 'letters', added numbers array

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 // Array of possible Characters
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+
 
 
 // Write password to the #password input
