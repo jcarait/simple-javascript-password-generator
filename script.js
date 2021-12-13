4d1c488ddb46314b22c25718580c91833ab8102e
Jonathan F-Carait <jonathan.carait@outlook.com>
2021-12-13 19:38:35 +1100

    added array for special chars

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var generateBtn = document.querySelector("#generate");
 // Array of possible Characters
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*"];
+
 
 
 
